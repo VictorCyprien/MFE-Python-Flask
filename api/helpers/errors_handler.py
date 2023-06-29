2367753b9fb37d4d29c893c3890f00b038dd8e54
VictorCyprien <victor.cyprien@limayrac.fr>
2023-06-29 10:40:17 +0200

    Add ErrorHandler class

--- api/helpers/errors_handler.py
+++ api/helpers/errors_handler.py
@@ -1,5 +1,6 @@
 from werkzeug.exceptions import BadRequest as WerkzeugBadRequest
 from werkzeug.exceptions import NotFound as WerkzeugNotFound
+from enum import Enum
 
 
 class BadRequest(WerkzeugBadRequest):
@@ -28,3 +29,9 @@ class NotFound(WerkzeugNotFound):
         if message:
             self.data = {}
             self.data["message"] = message
+
+
+class ErrorHandler(Enum):
+    USER_NOT_FOUND = "This user doesn't exist !"
+    EMAIL_ALREADY_USED = "This email is already used !"
+    UPDATE_USER_ERROR = "Something went wrong when updating the user, please try again !"
