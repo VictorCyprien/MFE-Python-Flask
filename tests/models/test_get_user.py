128db135bbab8a1271c59ccf7f437938a8102641
VictorCyprien <victor.cyprien@limayrac.fr>
2023-07-25 08:51:45 +0200

    Add tests

--- tests/models/test_get_user.py
+++ tests/models/test_get_user.py
@@ -1,5 +1,6 @@
 import pytest
-
+from datetime import datetime
+import pytz
 from mongoengine.errors import DoesNotExist, ValidationError
 
 from api.models.user import User
@@ -18,3 +19,11 @@ def test_model_get_user(app, victor: User, sayori: User):
 def test_model_get_user_wrong_id_format(app):
     with pytest.raises(ValidationError):
         User.get_by_id("MY_ID")
+
+
+def test_model_get_creation_and_update_time(app, victor: User):
+    creation_time = victor.creation_time
+    update_time = victor.update_time
+
+    assert creation_time == datetime(2000, 1, 1, 0, 0, tzinfo=pytz.utc)
+    assert update_time == datetime(2000, 1, 1, 0, 0, tzinfo=pytz.utc)
